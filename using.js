89c69e48719901321882c93beadcd02899d1f245
Alexandr Em <Alexandr Em>
2017-02-07 23:02:43 +0300

    Add using

---
+++ using.js
@@ -0,0 +1,22 @@
+	var waypointsvg = new Waypoint({
+
+		element: $(".section_5"),
+		handler: function(dir) {
+			
+			if (dir === "down") {
+
+				$(".section_5 .tc-item").each(function(index) {
+					var ths = $(this);
+					setTimeout(function() {
+						var myAnimation = new DrawFillSVG({
+							elementId: "tc-svg-" + index
+						});
+						ths.children(".tc-content").addClass("tc-content-on");
+					}, 500*index);
+				});
+
+			};
+			this.destroy();
+		},
+		offset: '35%'
+	});
